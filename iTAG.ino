9fe2bebdad4fbb139a7c694f88bd887238a32c4d
gnu <mr.jb.swe@gmail.com>
2018-01-14 16:48:43 +0100

    class iTAG

--- iTAG.ino
+++ iTAG.ino
@@ -13,65 +13,162 @@ U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset= *
 
 static void wifi_power_save(void);
 
-BLECharacteristic* pCharacteristic1 = 0;
-BLECharacteristic* pCharacteristic2 = 0;
-BLECharacteristic* pCharacteristic3 = 0;
-
-
-
 bool deviceConnected = false;
-uint8_t value = 0;
+
+
+ 
+class iTAG
+{
+  public:
+
+  void     init( const char* name );
+
+  void     set_btn( uint8_t btn );
+
+  void     set_batt( uint8_t batt );
+
+  uint8_t  _btn;
+  uint8_t  _batt;
+
+  protected:
+
+  enum
+  {
+      CH_BTN,
+      CH_ALERT,
+      CH_BATT,
+      CH_SZ
+  };
+
+  class AlertCallbacks: public BLECharacteristicCallbacks
+  {
+    public:
+      void onWrite(BLECharacteristic* pCh )
+      {
+  
+          Serial.print("Received Value: ");
+  
+         std::string rxValue=pCh->getValue();
+
+  // DEBUG
+         u8x8.setCursor(0, 2);
+          if (rxValue[0] == 2)
+             u8x8.print("ON  ");
+         else if (rxValue[0] == 0)
+            u8x8.print("OFF ");
+  
+      }
+  };
+
+  class ServerCallbacks: public BLEServerCallbacks
+  {
+  public:
+      void onConnect(BLEServer* pServer)
+      {
+          deviceConnected = true;
+      };
+      
+      void onDisconnect(BLEServer* pServer)
+      {
+          deviceConnected = false;
+          esp_deep_sleep_start();
+      }
+  };
+
+  
+ BLECharacteristic* _bl_char[CH_SZ];
+ ServerCallbacks    _cb_server;
+};
+
+void     
+iTAG::init(const char* name )
+{
+    
+    uint8_t new_mac[] = {0xFF, 0xFF, 0xFF, 0x08, 0x08, 0x06};
+    //srand(esp_random());for (int i = 0; i < sizeof(new_mac); i++) { new_mac[i] = rand() % 256;}
+    esp_base_mac_addr_set(new_mac);
+
+// Create the BLE Device
+    BLEDevice::init(name);
+
+// Create the BLE Server
+    BLEServer *pServer = BLEDevice::createServer();
+    pServer->setCallbacks(&_cb_server);
 
 // SIMPLE KEY OR BUTTON SERVICE
-#define S1_UUID    "0000ffe0-0000-1000-8000-00805f9b34fb"
-#define CHAR1_UUID "0000ffe1-0000-1000-8000-00805f9b34fb"
-
+    BLEService *pService1 = pServer->createService("0000ffe0-0000-1000-8000-00805f9b34fb");
+    _bl_char[CH_BTN] = pService1->createCharacteristic(
+                           "0000ffe1-0000-1000-8000-00805f9b34fb",
+                           BLECharacteristic::PROPERTY_NOTIFY |
+                           BLECharacteristic::PROPERTY_READ
+                       );
+ 
+ 
 // IMMEDIATE ALERT SERVICE
-#define S2_UUID    BLEUUID((uint16_t)0x1802)
-#define CHAR2_UUID BLEUUID((uint16_t)0x02a06)
+    BLEService *pService2 = pServer->createService(BLEUUID((uint16_t)0x1802));
+    _bl_char[CH_ALERT] = pService2->createCharacteristic(
+                           BLEUUID((uint16_t)0x02a06),
+                           BLECharacteristic::PROPERTY_NOTIFY |
+                           BLECharacteristic::PROPERTY_WRITE  |
+                           BLECharacteristic::PROPERTY_WRITE_NR
+                       );
+
+    _bl_char[CH_ALERT]->setCallbacks(new AlertCallbacks() );
 
 // BATTERY_SERVICE
-#define S3_UUID    BLEUUID((uint16_t)0x180F)
-#define CHAR3_UUID BLEUUID((uint16_t)0x2A19)
+    BLEService *pService3 = pServer->createService(BLEUUID((uint16_t)0x180F));
+   _bl_char[CH_BATT]  = pService3->createCharacteristic(
+                           BLEUUID((uint16_t)0x2A19),
+                           BLECharacteristic::PROPERTY_READ   |
+                           BLECharacteristic::PROPERTY_NOTIFY
+                       );
 
-class MyCallbacks: public BLECharacteristicCallbacks
-{
-    void onWrite(BLECharacteristic *pCharacteristic2)
-    {
+    _batt = 20;
+    _bl_char[CH_BATT]->setValue(&_batt,1);
 
-        Serial.print("Received Value: ");
-
-        std::string rxValue = pCharacteristic2->getValue();
-
-        u8x8.setCursor(0, 2);
-        if (rxValue[0] == 2)
-            u8x8.print("ON  ");
-        else if (rxValue[0] == 0)
-            u8x8.print("OFF ");
-    }
-};
+// Create a BLE Descriptor - not used in order to mimic iTag
+// pCharacteristic1->addDescriptor(new BLE2902());
+// pCharacteristic2->addDescriptor(new BLE2902());
+// pCharacteristic3->addDescriptor(new BLE2902());
 
 
-class MyServerCallbacks: public BLEServerCallbacks
-{
+    // Start the service
+    pService1->start();
+    pService2->start();
+    pService3->start();
 
-    void onConnect(BLEServer* pServer)
-    {
-        deviceConnected = true;
-        Serial.println("Device connected ...");
+// Start advertising
+   pServer->getAdvertising()->start();
+}
 
-    };
-    void onDisconnect(BLEServer* pServer)
-    {
-        deviceConnected = false;
-        Serial.println("Device disconnected ......");
-        Serial.println("ESP32 go to sleep for " + String(TIME_TO_SLEEP) + " s");
-        esp_deep_sleep_start();
-    }
-};
+
+ void     
+ iTAG::set_btn( uint8_t btn )
+ {
+     _btn = btn;
+     _bl_char[CH_BTN]->setValue(&_btn, 1);
+     _bl_char[CH_BTN]->notify();
+ }
+
+
+  void     
+ iTAG::set_batt( uint8_t batt )
+ {
+     _batt = batt;
+     _bl_char[CH_BATT]->setValue(&_batt, 1);
+     _bl_char[CH_BATT]->notify();
+ }
+
+
+
+ iTAG g_itag;
+
 
 void setup()
 {
+    
+
+     
     Serial.begin(115200);
 
 
@@ -88,67 +185,7 @@ void setup()
 
     esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
 
-    uint8_t new_mac[] = {0xFF, 0xFF, 0xFF, 0x08, 0x08, 0x06};
-    //srand(esp_random());for (int i = 0; i < sizeof(new_mac); i++) { new_mac[i] = rand() % 256;}
-    esp_base_mac_addr_set(new_mac);
-
-    // Create the BLE Device
-    BLEDevice::init("iTAG-EMU");
-
-    // Create the BLE Server
-    BLEServer *pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new MyServerCallbacks());
-
-    // Create the BLE Service   // Create a BLE Characteristic
-    BLEService *pService1 = pServer->createService(S1_UUID);
-    pCharacteristic1 = pService1->createCharacteristic(
-                           CHAR1_UUID,
-                           BLECharacteristic::PROPERTY_NOTIFY |
-                           BLECharacteristic::PROPERTY_READ
-                       );
-
-
-//  BLECharacteristic::PROPERTY_NOTIFY |
-// Create the BLE Service   // Create a BLE Characteristic
-
-
-    BLEService *pService2 = pServer->createService(S2_UUID);
-    pCharacteristic2 = pService2->createCharacteristic(
-                           CHAR2_UUID,
-                           BLECharacteristic::PROPERTY_NOTIFY |
-                           BLECharacteristic::PROPERTY_WRITE  |
-                           BLECharacteristic::PROPERTY_WRITE_NR
-                       );
-
-    pCharacteristic2->setCallbacks(new MyCallbacks());
-
-
-    BLEService *pService3 = pServer->createService(S3_UUID);
-    pCharacteristic3 = pService3->createCharacteristic(
-                           CHAR3_UUID,
-                           BLECharacteristic::PROPERTY_READ   |
-                           BLECharacteristic::PROPERTY_NOTIFY
-                       );
-
-
-
-// Create a BLE Descriptor - not used in order to mimic iTag
-// pCharacteristic1->addDescriptor(new BLE2902());
-// pCharacteristic2->addDescriptor(new BLE2902());
-// pCharacteristic3->addDescriptor(new BLE2902());
-
-
-    // Start the service
-    pService1->start();
-    pService2->start();
-
-
-   value = 20;
-   pCharacteristic3->setValue(&value,1);
-   pService3->start();
-
-// Start advertising
-    pServer->getAdvertising()->start();
+    g_itag.init("iTAG");
 
     delay(3000);
 
@@ -164,12 +201,10 @@ void setup()
 void loop()
 {
 
-    if (!digitalRead(0) )
+   if (!digitalRead(0) )
     {
-        value = 1;
-        Serial.printf("*** NOTIFY: %d ***\n", value);
-        pCharacteristic1->setValue(&value, 1);
-        pCharacteristic1->notify();
+       
+        g_itag.set_btn( 1 );
 
         while (!digitalRead(0))
         {
@@ -178,10 +213,7 @@ void loop()
     }
     else
     {
-        value = 0;
-        //Serial.printf("*** NOTIFY: %d ***\n", value);
-        // pCharacteristic1->setValue(&value, 1);
-        // pCharacteristic1->notify();
+
         delay(100);
     }
 }
